9f170129f9b3c6a97356cbe6fcae704f80d4227c
jcolombo <jcolombo@360psg.com>
2020-03-09 15:51:02 -0400

    Fix copyright blank line removal by IDE (hint: Just used periods)

--- src/Entity/Resource/Client.php
+++ src/Entity/Resource/Client.php
@@ -3,21 +3,21 @@
  * PHP SDK for the PaymoApp API
  * Package Source Code: https://github.com/jcolombo/paymo-api-php
  * Paymo API Documentation : https://github.com/paymoapp/api
- *
+ * .
  * MIT License
  * Copyright (c) 2020 - Joel Colombo <jc-dev@360psg.com>
- * Last Updated : 3/9/20, 3:44 PM
- *
+ * Last Updated : 3/9/20, 3:51 PM
+ * .
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * .
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * .
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
