49871e9e3f83f6f33bce4aa8a93c7f4e2c56b0cc
jcolombo <jcolombo@360psg.com>
2020-03-18 21:11:37 -0400

    Add Invoice payment resources

--- src/Entity/Resource/InvoicePayment.php
+++ src/Entity/Resource/InvoicePayment.php
@@ -32,44 +32,38 @@ namespace Jcolombo\PaymoApiPhp\Entity\Resource;
 use Jcolombo\PaymoApiPhp\Entity\AbstractResource;
 
 /**
- * Class Invoice
+ * Class InvoicePayment
  *
  * @package Jcolombo\PaymoApiPhp\Entity\Resource
  */
-class Invoice extends AbstractResource
+class InvoicePayment extends AbstractResource
 {
 
     /**
      * The user friendly name for error displays, messages, alerts, and logging
      */
-    public const LABEL = 'Invoice';
+    public const LABEL = 'Invoice Payment';
 
     /**
      * The entity key for associations and references between the package code classes
      */
-    public const API_ENTITY = 'invoice';
+    public const API_ENTITY = 'invoicepayment';
 
     /**
      * The path that is attached to the API base URL for the api call
      */
-    public const API_PATH = 'invoices';
+    public const API_PATH = 'invoicepayments';
 
     /**
      * The minimum properties that must be set in order to create a new entry via the API
      * To make an OR limit: 'propA|propB' = ONLY 1 of these. 'propA||propB' = AT LEAST 1 or more of these.
      */
-    public const REQUIRED_CREATE = ['client_id', 'currency'];
+    public const REQUIRED_CREATE = ['invoice_id', 'amount'];
 
     /**
      * The object properties that can only be read and never set, updated, or added to the creation
      */
-    public const READONLY = ['id', 'created_on', 'updated_on', 'subtotal', 'total',
-        'tax_amount', 'tax2_amount', 'invoice_id', 'permalink', 'pdf_link',
-        'language', // Added to readonly since it has been deprecated
-        'reminder_1_sent', 'reminder_2_sent', 'reminder_3_sent',
-        // Undocumented
-        'discount_amount', 'download_token'
-    ];
+    public const READONLY = ['id', 'created_on', 'updated_on', 'invoice_id'];
 
     /**
      * An array of properties from the readonly array that can be set during creation but not after
@@ -82,10 +76,7 @@ class Invoice extends AbstractResource
      * TRUE = the include is a list of multiple entities. FALSE = a single object is associated with the entity
      */
     public const INCLUDE_TYPES = [
-        'client' => false,
-        'invoicepayments' => true,
-        'invoiceitems' => true,
-        'invoicetemplate' => false
+        'invoice' => false
     ];
 
     /**
@@ -95,44 +86,11 @@ class Invoice extends AbstractResource
         'id' => 'integer',
         'created_on' => 'datetime',
         'updated_on' => 'datetime',
-        'number' => 'text',
-        'client_id' => 'resource:client',
-        'template_id' => 'resource:invoicetemplate',
-        'status' => 'enum:draft|sent|viewed|paid|void',
-        'currency' => 'text',
+        'invoice_id' => 'resource:invoice',
+        'amount' => 'decimal',
         'date' => 'date',
-        'due_date' => 'date',
-        'subtotal' => 'decimal',
-        'total' => 'decimal',
-        'tax' => 'decimal',
-        'tax_amount' => 'decimal',
-        'tax2' => 'decimal',
-        'tax2_amount' => 'decimal',
-        'discount' => 'decimal',
-        'discount_amount' => 'decimal',
-        'tax_on_tax' => 'boolean',
-        'language' => 'text',  // Deprecated as used by Invoice Templates
-        'bill_to' => 'text',
-        'company_info' => 'text',
-        'footer' => 'text',
-        'notes' => 'text',
-        'outstanding' => 'decimal',
-        'tax_text' => 'text',
-        'tax2_text' => 'text',
-        'discount_text' => 'text',
-        'title' => 'text',
-        'pay_online' => 'boolean',
-        'reminder_1_sent' => 'boolean',
-        'reminder_2_sent' => 'boolean',
-        'reminder_3_sent' => 'boolean',
-        'permalink' => 'url',
-        'pdf_link' => 'url',
-        // Undocumented Props
-        'download_token' => 'text',
-        'active' => 'boolean',
-        'delivery_date' => 'date',
-        'options' => 'object',
-        'token' => 'text'
+        'notes' => 'text'
+        // Undocumented Props        
     ];
 
     /**
