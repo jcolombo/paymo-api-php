311eb465dee2118c3c888522ef32b04d46131b6c
jcolombo <jcolombo@360psg.com>
2020-03-19 13:43:18 -0400

    Start a file to use as a typescript TYPES file for applications

---
+++ src/.resources/typescript.data-types.ts
@@ -0,0 +1,46 @@
+/*
+ * PHP SDK for the PaymoApp API
+ * Package Source Code: https://github.com/jcolombo/paymo-api-php
+ * Paymo API Documentation : https://github.com/paymoapp/api
+ * .
+ * MIT License
+ * Copyright (c) 2020 - Joel Colombo <jc-dev@360psg.com>
+ * Last Updated : 3/19/20, 1:43 PM
+ * .
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * .
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * .
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// START DATA TYPES for Typescript use if calling PHP SDK via Typescript JSON (for example with React/Redux calls)
+
+export interface PaymoBooking {
+  id: number,
+  created_on: string,
+  updated_on: string,
+  user_task_id: number,
+  start_date: string,
+  end_date: string,
+  hours_per_date: number,
+  description: string,
+  creator_id: number,
+  user_id: number,
+  start_time: string,
+  end_time: string,
+  booked_hours: number
+}
+
